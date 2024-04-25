ed24f05cc750677aa8c34be0cc9516c7459630cc
William Floberg <williamfloberg7@gmail.com>
2024-04-25 02:21:59 +0000

    finished code comments and game

--- projects/runtime/js/levelData.js
+++ projects/runtime/js/levelData.js
@@ -11,53 +11,68 @@ var makeLevelData = function (window) {
     // TODO 12: change the below data
     var levelData = [
       {
-        name: "Stage 1",
-        number: 1,
-        speed: -20,
-        gameItems: [
-          { type: "sawblade", x: 400, y: groundY },
-          { type: "sawblade", x: 600, y: groundY },
-          { type: "sawblade", x: 800, y: groundY },
-          { type: "laser", x: 1200, y: groundY - 130 },
-          { type: "enemy", x: 1500, y: groundY - 50},
-          {type: "buddy", x: 1700, y: groundY - 100},
-          { type: "sawblade", x: 2000, y: groundY},
-          {type: "buddy", x: 2150, y: groundY - 100},
-          { type: "sawblade", x: 2300, y: groundY},
-          { type: "laser", x: 2735, y: groundY - 130 },
-          { type: "laser", x: 2845, y: groundY - 130 },
-          { type: "laser", x: 2955, y: groundY - 130 },
-          {type: "buddy", x: 3100, y: groundY - 100},
-          { type: "enemy", x: 3400, y: groundY - 50},
-          { type: "enemy", x: 3600, y: groundY - 50},
-          { type: "enemy", x: 3800, y: groundY - 50},
-          {type: "buddy", x: 4000, y: groundY - 100},
-          { type: "sawblade", x: 4200, y: groundY},
-          { type: "sawblade", x: 4400, y: groundY},
-          { type: "sawblade", x: 4600, y: groundY},
-          { type: "marker", x: 5000, y: groundY - 100 },
+        name: "Stage 1", //sets level name
+        number: 1, //sets level number
+        speed: -20, //sets level speed
+        gameItems: [ //lists items in this stage
+          { type: "sawblade", x: 400, y: groundY }, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 600, y: groundY }, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 800, y: groundY }, //adds a spike at the set x and y coords
+          { type: "laser", x: 1200, y: groundY - 130 }, //adds a laser at the set x and y coords
+          { type: "enemy", x: 1500, y: groundY - 50}, //adds a turret at the set x and y coords
+          {type: "buddy", x: 1700, y: groundY - 100}, //adds a reward at the set x and y coords
+          { type: "sawblade", x: 2000, y: groundY}, //adds a spike at the set x and y coords
+          {type: "buddy", x: 2150, y: groundY - 100}, //adds a reward at the set x and y coords
+          { type: "sawblade", x: 2300, y: groundY}, //adds a spike at the set x and y coords
+          { type: "laser", x: 2735, y: groundY - 130 }, //adds a laser at the set x and y coords
+          { type: "laser", x: 2845, y: groundY - 130 }, //adds a laser at the set x and y coords
+          { type: "laser", x: 2955, y: groundY - 130 }, //adds a laser at the set x and y coords
+          {type: "buddy", x: 3100, y: groundY - 100}, //adds a reward at the set x and y coords
+          { type: "enemy", x: 3400, y: groundY - 50}, //adds a turret at the set x and y coords
+          { type: "enemy", x: 3600, y: groundY - 50}, //adds a turret at the set x and y coords
+          { type: "enemy", x: 3800, y: groundY - 50}, //adds a turret at the set x and y coords
+          {type: "buddy", x: 4000, y: groundY - 100}, //adds a reward at the set x and y coords
+          { type: "sawblade", x: 4200, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4400, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4600, y: groundY}, //adds a spike at the set x and y coords
+          { type: "marker", x: 5000, y: groundY - 100 }, //adds a marker at the set x and y coords
         ],
       },
       {
-        name: "Stage 2",
-        number: 2,
-        speed: -20,
-        gameItems: [
-          { type: "sawblade", x: 500, y: groundY},
-          { type: "laser", x: 700, y: groundY - 130 },
-          { type: "sawblade", x: 900, y: groundY},
-          {type: "buddy", x: 875, y: groundY - 130},
-          { type: "laser", x: 1100, y: groundY - 130 },
-          { type: "sawblade", x: 1300, y: groundY},
-          { type: "dummy", x: 1700, y: groundY - 50},
-          { type: "enemy", x: 1900, y: groundY - 50},
-          { type: "dummy", x: 2100, y: groundY - 50},
-          { type: "enemy", x: 2300, y: groundY - 50},
-          { type: "enemy", x: 2500, y: groundY - 50},
-          {type: "buddy", x: 3000, y: groundY - 100},
-          {type: "buddy", x: 3200, y: groundY - 100},
-          {type: "buddy", x: 3400, y: groundY - 100},
-          {type: "buddy", x: 3600, y: groundY - 100},
+        name: "Stage 2", //sets level number
+        number: 2, //sets level number
+        speed: -20, //sets level speed
+        gameItems: [ //lists items in this stage
+          { type: "sawblade", x: 500, y: groundY}, //adds a spike at the set x and y coords
+          { type: "laser", x: 700, y: groundY - 130 }, //adds a laser at the set x and y coords
+          { type: "sawblade", x: 900, y: groundY}, //adds a spike at the set x and y coords
+          {type: "buddy", x: 875, y: groundY - 130}, //adds a reward at the set x and y coords
+          { type: "laser", x: 1100, y: groundY - 130 }, //adds a laser at the set x and y coords
+          { type: "sawblade", x: 1300, y: groundY}, //adds a spike at the set x and y coords
+          { type: "dummy", x: 1700, y: groundY - 50}, //adds wheatley at the set x and y coords
+          { type: "enemy", x: 1900, y: groundY - 50}, //adds a turret at the set x and y coords
+          { type: "dummy", x: 2100, y: groundY - 50}, //adds wheatley at the set x and y coords
+          { type: "enemy", x: 2300, y: groundY - 50}, //adds a turret at the set x and y coords
+          { type: "enemy", x: 2500, y: groundY - 50}, //adds a turret at the set x and y coords
+          {type: "buddy", x: 3000, y: groundY - 100}, //adds a reward at the set x and y coords
+          {type: "buddy", x: 3200, y: groundY - 100}, //adds a reward at the set x and y coords
+          {type: "buddy", x: 3400, y: groundY - 100}, //adds a reward at the set x and y coords
+          {type: "buddy", x: 3600, y: groundY - 100}, //adds a reward at the set x and y coords
+          { type: "sawblade", x: 4000, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4150, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4300, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4450, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4600, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4750, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 4900, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 5050, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 5200, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 5350, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 5500, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 5650, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 5800, y: groundY}, //adds a spike at the set x and y coords
+          { type: "sawblade", x: 5950, y: groundY}, //adds a spike at the set x and y coords
+
         ],
       },
     ];
--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -14,76 +14,76 @@ var runLevels = function (window) {
     var levelData = window.opspark.levelData;
 
     // set this to true or false depending on if you want to see hitzones
-    game.setDebugMode(true);
+    game.setDebugMode(false);
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
-    function createSawBlade(x, y){ // creates sawBlade function (got lazy and made this another obstacle w/o renaming it)
-      var hitZoneSize = 20; // sets the saw's hitbox size
-      var damageFromObstacle = 125; // sets the saw's damage
-      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); // 
-      sawBladeHitZone.x = x; // sets the hitbox's x coordinate
-      sawBladeHitZone.y = y; // sets the hitbox's y coordinate
-      game.addGameItem(sawBladeHitZone); // adds the hitbox into the game 
-      var obstacleImage = draw.bitmap("img/sawblade.png"); // gives the obstacle an image 
-      sawBladeHitZone.addChild(obstacleImage); //     adds the image as a child of the hitzone
-      obstacleImage.x = -25 // sets the x coordinate of the sawblade in relevance to the hitbox
-      obstacleImage.y = -45 // sets the y coordinate of the sawblade in relevance to the hitbox
-      obstacleImage.scaleX = 0.06
-      obstacleImage.scaleY = 0.06
-      sawBladeHitZone.velocityX = -5
+    function createSawBlade(x, y){ //creates sawBlade function (got lazy and made this another obstacle w/o renaming it)
+      var hitZoneSize = 20; //sets the spikes's hitbox size
+      var damageFromObstacle = 125; //sets the saw's damage
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); //creates a "sawblade" (spike, i was lazy) hit zone obstacle 
+      sawBladeHitZone.x = x; //sets the hitbox's x coordinate
+      sawBladeHitZone.y = y; //sets the hitbox's y coordinate
+      game.addGameItem(sawBladeHitZone); //adds the hitbox into the game 
+      var obstacleImage = draw.bitmap("img/sawblade.png"); //gives the obstacle an image 
+      sawBladeHitZone.addChild(obstacleImage); //adds the image as a child of the hitzone
+      obstacleImage.x = -25 //sets the x coordinate of the sawblade in relevance to the hitbox
+      obstacleImage.y = -45 //sets the y coordinate of the sawblade in relevance to the hitbox
+      obstacleImage.scaleX = 0.06 //scales down the image so it fits over the hitzone on the x coordinate
+      obstacleImage.scaleY = 0.06 //scales down the image so it fits over the hitzone on the y coordinate
+      sawBladeHitZone.velocityX = -5 //sets the velocity of the hitzone to move leftward 
     }
-    function createLaser(x, y){
-      var hitZoneSize = 50;
-      var damageFromObstacle = 125
-      var laserHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-      laserHitZone.x = x;
-      laserHitZone.y = y;
-      game.addGameItem(laserHitZone);
-      var obstacleImage = draw.bitmap("img/laser.png");
-      laserHitZone.addChild(obstacleImage);
-      obstacleImage.x = -65
-      obstacleImage.y = 0
-      obstacleImage.scaleX = .07
-      obstacleImage.scaleY = .07
-      laserHitZone.velocityX = -5
+    function createLaser(x, y){ //creates a laser object at the specified x and y coord
+      var hitZoneSize = 50; //defines the size of the invisible hit zone for the laser
+      var damageFromObstacle = 125 //defines the damage dealt when the laser hits something
+      var laserHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); //creates an invisible obstacle object with the defined size and damage
+      laserHitZone.x = x; //sets the x position of the laser object
+      laserHitZone.y = y; //sets the y position of the laser object
+      game.addGameItem(laserHitZone); //adds the laser object to the game
+      var obstacleImage = draw.bitmap("img/laser.png"); //loads img/laser.png
+      laserHitZone.addChild(obstacleImage); //sets img/laser.png to the laserHitZone
+      obstacleImage.x = -65 //repositions the image to the left of the hitzone to fit
+      obstacleImage.y = 0 //positions the image ontop of the hitzone
+      obstacleImage.scaleX = .07 //scales down the image so it fits over the hitzone on the x coordinate
+      obstacleImage.scaleY = .07 //scales down the image so it fits over the hitzone on the y coordinate
+      laserHitZone.velocityX = -5 //sets the velocity of the hitzone to move leftward
     }
-    function createEnemy(x, y){ // creates createEnemy function
+    function createEnemy(x, y){ // creates an enemy object at the specified x and y coords
       var enemy = game.createGameItem("enemy", 30); // creates a variable enemy and assigns it parameters
-      var redSquare = draw.bitmap('img/turret.png'); // creates a variable and draws it
-      redSquare.x = -60; // sets the x coordinate of the enemy png in relevance to the hitbox
-      redSquare.y = -45; // sets the y coordinate of the enemy png in relevance to the hitbox
-      enemy.addChild(redSquare); // adds redSquare as a child of enemy
-      enemy.x = x; // assigns the enemy to the functions x parameter
-      enemy.y = y // assigns the enemy to the functions y parameter
-      game.addGameItem(enemy); // adds the enemy into the game
-      enemy.velocityX = -5; // sets the enemy's velocity to -5 so it moves left
-      enemy.scaleX = -0.7
-      enemy.scaleY = 0.7
-      enemy.onPlayerCollision = function () { // makes enemy.onPlayerCollision into a function
-        enemy.fadeOut(); // makes enemy fade out when shot
-        game.changeIntegrity(-51); // makes HalleBot loose 10 health on enemy collision
+      var redSquare = draw.bitmap('img/turret.png'); //loads img/turret.png
+      redSquare.x = -60; //sets the x position of img/turret.png
+      redSquare.y = -45; //sets the y position of img/turret.png
+      enemy.addChild(redSquare); //assigns the turret image to enemy
+      enemy.x = x; //sets the x position of the enemy
+      enemy.y = y //sets the y position of the enemy
+      game.addGameItem(enemy); //adds the enemy object to the game
+      enemy.velocityX = -5; //sets the velocity of the enemy to move leftward
+      enemy.scaleX = -0.7 //scales down the image so it fits over the hitzone on the x coordinate
+      enemy.scaleY = 0.7 //scales down the image so it fits over the hitzone on the y coordinate
+      enemy.onPlayerCollision = function () { //makes enemy.onPlayerCollision into a function
+        enemy.fadeOut(); //makes enemy fade out when shot
+        game.changeIntegrity(-51); //makes HalleBot loose 10 health on enemy collision
       };
     
-      enemy.onProjectileCollision = function (){ // makes enemy.onProjcetileCollision into a function
+      enemy.onProjectileCollision = function (){ //defines what happens when the enemy collides with the player
         game.increaseScore(500); // increases score by 100 when enemies shot
         enemy.fadeOut(); // makes enemy fade out when shot
       };
     
     }
-    function createDummy(x, y){ // creates createEnemy function
-      var enemy = game.createGameItem("dummy", 80); // creates a variable enemy and assigns it parameters
-      var redSquare = draw.bitmap('img/wheatley.png'); // creates a variable and draws it
-      redSquare.x = -100; // sets the x coordinate of the enemy png in relevance to the hitbox
-      redSquare.y = -100; // sets the y coordinate of the enemy png in relevance to the hitbox
-      enemy.addChild(redSquare); // adds redSquare as a child of enemy
-      enemy.x = x; // assigns the enemy to the functions x parameter
-      enemy.y = y // assigns the enemy to the functions y parameter
-      game.addGameItem(enemy); // adds the enemy into the game
-      enemy.velocityX = -5; // sets the enemy's velocity to -5 so it moves left
-      enemy.scaleX = -0.35
-      enemy.scaleY = 0.35
-      enemy.onPlayerCollision = function () { // makes enemy.onPlayerCollision into a function
+    function createDummy(x, y){ // creates my other enemy object at the specified x and y coords
+      var enemy = game.createGameItem("dummy", 80); //creates an enemy variable and gives it parameters
+      var redSquare = draw.bitmap('img/wheatley.png'); //loads img/wheatley.png
+      redSquare.x = -100; //sets the x coordinate of the enemy png in relevance to the hitbox
+      redSquare.y = -100; //sets the y coordinate of the enemy png in relevance to the hitbox
+      enemy.addChild(redSquare); //adds redSquare as a child of enemy
+      enemy.x = x; //sets the x position of the enemy
+      enemy.y = y //sets the y position of the enemy
+      game.addGameItem(enemy); //adds the enemy into the game
+      enemy.velocityX = -5; //sets the enemy's velocity to move leftward
+      enemy.scaleX = -0.35 //scales down the image on its x coord so it fits over the hitbox
+      enemy.scaleY = 0.35 //scales down the image on its y coord so it fits over the hitbox
+      enemy.onPlayerCollision = function () { //defines what happens when the enemy collides with the player
         game.changeIntegrity(-101); // makes HalleBot loose 10 health on enemy collision
         enemy.fadeOut(); // makes enemy fade out when shot
       };
@@ -93,65 +93,65 @@ var runLevels = function (window) {
         enemy.fadeOut(); // makes enemy fade out when shot
       };
     }
-    function createReward(x, y){ // creates a reward fuction
-      var reward = game.createGameItem("buddy", 50); // creates a variable named award and assigned it to game.CreateGameItem
-      var blueSquare = draw.bitmap('img/companion.png'); // creats a variable and draws it in the game
-      blueSquare.x = -25; // sets the squares x coordinate
-      blueSquare.y = -25; // sets the squares y coordinate
-      reward.addChild(blueSquare); // adds the blueSquare as a child of rewards
-      reward.x = x; // sets the blueSquares x coordinate
-      reward.y = y // sets the blueSquares y coordinate
-      game.addGameItem(reward); // adds the reward to the game
-      reward.velocityX = -5; // makes the reward move to the left at a velocity of 5
-      reward.scaleX = .3
-      reward.scaleY = .3
-      reward.onPlayerCollision = function () { // makes reward.onPlayerCollision into a function
+    function createReward(x, y){ //creates a reward object at the specified x and y coords
+      var reward = game.createGameItem("buddy", 50); ///creates an award variable and gives it parameters
+      var blueSquare = draw.bitmap('img/companion.png'); //loads img/companion.png
+      blueSquare.x = -25; //sets the x coordinate of the reward png in relevance to the hitbox
+      blueSquare.y = -25; //sets the y coordinate of the reward png in relevance to the hitbox
+      reward.addChild(blueSquare); //adds blueSquare as a child of reward
+      reward.x = x; //sets the x position of the enemy
+      reward.y = y //sets the y position of the enemy
+      game.addGameItem(reward); //adds the reward in to the game
+      reward.velocityX = -5; //makes the reward move to the left at a velocity of 5
+      reward.scaleX = .3 //scales down the image on its x coord so it fits over the hitbox
+      reward.scaleY = .3 //scales down the image on its y coord so it fits over the hitbox
+      reward.onPlayerCollision = function () { //makes reward.onPlayerCollision into a function
         game.changeIntegrity(+10); // makes HalleBot gain 10 health on reward collision
-        game.increaseScore(500)
+        game.increaseScore(500) //increases the score by 500 on reward collision
         reward.shrink(); // makes the reward shrink after contacting the player
       };
     }
-    function createMarker(x,y){
-      var marker = game.createGameItem("marker", 25); // creates a variable named marker and assigns it parameters
-      var greenSquare = draw.bitmap('img/storage.png'); // draws the square representing the marker
-      greenSquare.x = -25; // sets the reward's x position in relevance to it's hitbox
-      greenSquare.y = -25; // sets the reward's y position in relevance to it's hitbox
+    function createMarker(x,y){ //creates a marker object at the specified x and y coords
+      var marker = game.createGameItem("marker", 25); //creates a marker variable and assigns it parameters
+      var greenSquare = draw.bitmap('img/storage.png'); //loads img/storage.png
+      greenSquare.x = -25; //sets the reward's x position in relevance to it's hitbox
+      greenSquare.y = -25; //sets the reward's y position in relevance to it's hitbox
       marker.addChild(greenSquare); // adds greenSquare to a child of marker
-      marker.x = x; // sets the x coordinate of the marker
-      marker.y = y; // sets the y coordinate of the marker
-      game.addGameItem(marker); // adds the marker to the game
-      marker.scaleX = .3 // scales the marker png to fit its x axis
-      marker.scaleY = .3 // scales the marker png to fit its y axis
-      marker.velocityX = -5; // makes the marker move to the left at a velocity of 5
+      marker.x = x; //sets the x coords of the marker
+      marker.y = y; //sets the y coords of the marker
+      game.addGameItem(marker); //adds the marker to the game
+      marker.scaleX = .3 //scales the marker png to fit its x axis
+      marker.scaleY = .3 //scales the marker png to fit its y axis
+      marker.velocityX = -5; //makes the marker move to the left at a velocity of 5
       
-      marker.onPlayerCollision = function () { // makes marker.onPlayerCollision into a function
-      game.increaseScore(1000)
-      marker.shrink(); // makes the marker shrink when 
-      startLevel();
+      marker.onPlayerCollision = function () { //defines what happens when Hallebot collides with the marker
+      game.increaseScore(1000) //increases the score by 1000 on marker collision
+      marker.shrink(); //makes the marker shrink on collision
+      startLevel(); //starts the next level on marker collision
       }
     }
-    function startLevel() {
+    function startLevel() { //defines what happens when a level is started
       // TODO 13 goes below here
-      var level = levelData[currentLevel];
-      var levelObjects = level.gameItems
-      for(var i = 0; i < levelObjects.length; i++){
-        var item = levelObjects[i];
-        if(item.type === "sawblade"){
-          createSawBlade(item.x, item.y)
+      var level = levelData[currentLevel]; //gets data for the current level from the level data array
+      var levelObjects = level.gameItems //extracts the game items array from the current level data
+      for(var i = 0; i < levelObjects.length; i++){ //loops through all game items in the current level
+        var item = levelObjects[i]; //gets the current game item data
+        if(item.type === "sawblade"){ //creates a spike at the item's position
+          createSawBlade(item.x, item.y) 
         }
-        if(item.type === "enemy"){
+        if(item.type === "enemy"){ //creates a turret at the item's position
           createEnemy(item.x, item.y)
         }
-        if(item.type === "buddy"){
+        if(item.type === "buddy"){ //creates a reward at the item's position
           createReward(item.x, item.y)
         }
-        if(item.type === "marker"){
+        if(item.type === "marker"){ //creates a marker at the item's position
           createMarker(item.x, item.y)
         }
-        if(item.type === "laser"){
+        if(item.type === "laser"){//creates a laser at the item's position
           createLaser(item.x, item.y)
         }
-        if(item.type === "dummy"){
+        if(item.type === "dummy"){//creates a wheatley enemy at the item's position
           createDummy(item.x, item.y)
         }
       }
